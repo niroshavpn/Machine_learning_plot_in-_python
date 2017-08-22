1adfadf1aa7a05f601eef97376440ce62d98c8ed
niroshavpn <niroshavpn@gmail.com>
2017-08-22 16:58:59 -0400

    Add files via upload

---
+++ ML-proj-1.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (10.0, 8.0)
+import seaborn as sns
+from scipy import stats
+from scipy.stats import norm
+train = pd.read_csv("/Users/suresh/Documents/NIROSHA/Python-ML-projects/train.csv")
+test = pd.read_csv("/Users/suresh/Documents/NIROSHA/Python-ML-projects/test.csv")
+miss = train.isnull().sum()/len(train)
+
+miss = miss.to_frame()
+miss.columns = ['count']
+miss.index.names = ['Name']
+miss['Name'] = miss.index
+sns.set(style="whitegrid", color_codes=True)
+sns.barplot(x = 'Name', y = 'count', data=miss)
+plt.xticks(rotation = 90)
+sns.plt.show()
